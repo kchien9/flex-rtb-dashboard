a406fb50ab67848efdd6d4b965c022bc08caed66
kchien9 <kevin.chien@getflex.com>
2026-07-28 21:34:34 -0400

    Add segment_bucket to closed_won_by_rep.sql for the Segment/Team consolidation
    Kevin: "consolidate the segment and team and rep view... have the team
    just go into the segment view... these are mostly the same but smb will
    split out into smb 1 and smb 2." Confirmed: segment_bucket and
    team_bucket are the same grouping except SMB (Strategic = Dana's Team
    exactly, MM/Ent = Brandon's Team exactly, House Accounts/Not Set have no
    team_bucket at all). Added segment_bucket + a Segment filter so the UI
    can drill Segment -> Rep directly for 4 of the 5 segments, only showing
    an intermediate Team sub-level for SMB specifically.

--- queries/closed_won_by_rep.sql
+++ queries/closed_won_by_rep.sql
@@ -21,6 +21,18 @@
 -- drill-down should never introduce a pacing comparison the parent card doesn't have either.
 --
 -- FILTER ESCAPING -- same apostrophe risk as every value filter in this repo.
+--
+-- SEGMENT/TEAM CONSOLIDATION (added 2026-07-28) -- Kevin: "consolidate the segment and team
+-- and rep view... have the team just go into the segment view... these are mostly the same
+-- but smb will split out into smb 1 and smb 2." Confirmed: segment_bucket and team_bucket are
+-- literally the same underlying grouping except SMB -- Strategic segment = Dana's Team
+-- exactly (same STATIC_TEAM_NAME values), MM/Ent = Brandon's Team exactly, House Accounts/
+-- Not Set have no team_bucket at all. Only SMB splits into two real teams (Sebastian's/
+-- Rory's). Added segment_bucket here so the UI can drill Segment -> Rep DIRECTLY for
+-- Strategic/MM/Ent/House Accounts/Not Set (no team level needed, it'd just repeat the
+-- segment), and only show an intermediate Team sub-level when SMB specifically is clicked
+-- (Sebastian's vs. Rory's, via performance_cube.sql's existing team_bucket grouping filtered
+-- to segment_bucket='SMB' -- no new query needed for that intermediate step).
 
 WITH current_bp AS (
     SELECT IFF(DAY(CURRENT_DATE()) <= 4,
@@ -64,7 +76,15 @@ base AS (
             WHEN e.TEAM_NAME = 'SMB Account Executives 2' THEN 'Rory''s Team'
             WHEN e.TEAM_NAME IN ('Strategic Team', 'Cory''s Team', 'Heidi''s Team') THEN 'Dana''s Team'
             ELSE NULL
-        END AS team_bucket
+        END AS team_bucket,
+        CASE
+            WHEN e.TEAM_NAME = 'Brandon''s Team' THEN 'MM/Ent'
+            WHEN e.TEAM_NAME IN ('Strategic Team', 'Cory''s Team', 'Heidi''s Team') THEN 'Strategic'
+            WHEN e.TEAM_NAME IN ('SMB Account Executives', 'SMB Account Executives 1', 'SMB Account Executives 2') THEN 'SMB'
+            WHEN e.TEAM_NAME = 'House Accounts' THEN 'House Accounts'
+            WHEN e.TEAM_NAME IS NULL THEN 'Not Set'
+            ELSE NULL
+        END AS segment_bucket
     FROM FLEX.SALES.FCT_CRM_OPPORTUNITY o
     LEFT JOIN FLEX.MART.DIM_EMPLOYEE_HISTORY e ON o.OWNER_SK = e.EMPLOYEE_SK AND e.IS_CURRENT = TRUE
     WHERE o.OPPORTUNITY_TYPE IN ('New Logo', 'Expansion', 'Move In')
@@ -73,11 +93,13 @@ SELECT
     p.period,
     b.rep,
     b.team_bucket,
+    b.segment_bucket,
     SUM(IFF(b.IS_CLOSED_WON AND b.CLOSED_AT_UTC BETWEEN p.start_date AND p.end_date, b.FLEX_UNIT_COUNT, 0)) AS units
 FROM bp_periods p
 JOIN base b ON TRUE
 WHERE b.rep IS NOT NULL
-  {{#Team.value}} AND b.team_bucket = '{{Team.value}}' {{/Team.value}}
-GROUP BY 1, 2, 3
+  {{#Team.value}}     AND b.team_bucket = '{{Team.value}}'       {{/Team.value}}
+  {{#Segment.value}}  AND b.segment_bucket = '{{Segment.value}}' {{/Segment.value}}
+GROUP BY 1, 2, 3, 4
 HAVING units > 0
 ORDER BY p.period, units DESC;
